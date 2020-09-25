18176000175955d8b5ad636b12c5ebd33108108f
Vamsi <playstore.vamsi@gmail.com>
2020-09-25 18:32:50 -0400

    Update country details screen UI

--- app/src/main/java/com/vamsi/worldcountriesinformation/ui/countrydetails/CountryDetailsFragment.kt
+++ app/src/main/java/com/vamsi/worldcountriesinformation/ui/countrydetails/CountryDetailsFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -29,7 +28,7 @@ class CountryDetailsFragment: BaseFragment(), OnMapReadyCallback {
     private lateinit var binding: FragmentCountryDetailsBinding
     private lateinit var googleMap: GoogleMap
 
-    private lateinit var countryDetails: Country
+    private lateinit var country: Country
 
     companion object {
         fun newInstance(bundle: Bundle) = CountryDetailsFragment().apply {
@@ -50,6 +49,10 @@ class CountryDetailsFragment: BaseFragment(), OnMapReadyCallback {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        val countryDetails = mapCountryDetailsModel(this.arguments)
+        val supportMapFragment =
+            childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        supportMapFragment?.getMapAsync(this)
         adapter = CountryDetailsAdapter()
         binding.apply {
             viewModel = countryDetailsViewModel
@@ -57,28 +60,35 @@ class CountryDetailsFragment: BaseFragment(), OnMapReadyCallback {
             countryDetailsList.adapter = adapter
         }
 
-        val supportMapFragment =
-            childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-        supportMapFragment?.getMapAsync(this)
-
-        val countryDetails = mapCountryDetailsModel(this.arguments)
         adapter.submitList(countryDetails)
     }
 
     private fun mapCountryDetailsModel(bundle: Bundle?): List<CountryDetailsModel>? {
         return bundle?.let {
-            countryDetails = it.getSerializable(Constants.COUNTRY_DETAILS) as Country
+            country = it.getSerializable(Constants.COUNTRY_DETAILS) as Country
             val countryDetailsList = mutableListOf<CountryDetailsModel>()
-            countryDetailsList.add(
-                CountryDetailsModel(
-                    getString(R.string.country_name_label),
-                    countryDetails.name
+            countryDetailsList.add(CountryDetailsModel(getString(R.string.country_name_label),
+                    country.name
                 )
             )
-            countryDetailsList.add(
-                CountryDetailsModel(
-                    getString(R.string.capital_city_label),
-                    countryDetails.capital
+            countryDetailsList.add(CountryDetailsModel(getString(R.string.capital_city_label),
+                    country.capital
+                )
+            )
+            countryDetailsList.add(CountryDetailsModel(getString(R.string.population_label),
+                    country.population.toString()
+                )
+            )
+            countryDetailsList.add(CountryDetailsModel(getString(R.string.calling_code_label),
+                    country.callingCode
+                )
+            )
+            countryDetailsList.add(CountryDetailsModel(getString(R.string.language_label),
+                    country.languages.map { language -> language.name }.joinToString(", ")
+                )
+            )
+            countryDetailsList.add(CountryDetailsModel(getString(R.string.currency_label),
+                    country.currencies.map { currency -> currency.name }.joinToString(", ")
                 )
             )
             return@let countryDetailsList
@@ -89,12 +99,14 @@ class CountryDetailsFragment: BaseFragment(), OnMapReadyCallback {
         this.googleMap = googleMap
 
         // Add a marker and move the camera
-        val countryLocation = LatLng(countryDetails.latitude, countryDetails.longitude)
-        googleMap.addMarker(
-            MarkerOptions()
-                .position(countryLocation)
-                .title("Marker in ${countryDetails.name}")
-        )
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(countryLocation))
+        if (country.latitude != 0.0 && country.longitude != 0.0) {
+            val countryLocation = LatLng(country.latitude, country.longitude)
+            googleMap.addMarker(
+                MarkerOptions()
+                    .position(countryLocation)
+                    .title("Marker in ${country.name}")
+            )
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(countryLocation))
+        }
     }
 }
